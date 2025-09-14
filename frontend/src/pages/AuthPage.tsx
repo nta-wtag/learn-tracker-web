f5943b92aadc95d0e301215e550e96786a8d69d1
nta-wtag <noushin.tasnim@welldev.io>
2025-09-14 13:17:28 +0600

    Add auth page animations

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import loginImg from "../assets/Education-Isometric-Illustration.jpg";
 import registerImg from "../assets/42B2E4EC-58E6-4E91-9CA5-B570A5634F8F_1_201_a.jpeg";
 import Login from "../components/login/Login";
@@ -17,11 +18,17 @@ function AuthPage() {
         <div className="w-full h-screen relative flex px-64 text-[#2e214a] font-[poppins]">
             <div className="w-full h-full flex justify-center">
                 <div className="w-1/2 h-full flex items-center">
-                    <img
-                        src={isLogin ? loginImg : registerImg}
-                        className="w-full p-4 h-1/2 object-contain"
-                        alt="auth"
-                    />
+                    <AnimatePresence mode="wait">
+                        <motion.img
+                            key={isLogin ? "login-img" : "register-img"}
+                            src={isLogin ? loginImg : registerImg}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className="w-full p-4 h-1/2 object-contain"
+                        />
+                    </AnimatePresence>
                 </div>
                 <div className="w-1/2 h-full flex flex-col items-center justify-center z-10 bg-white">
                     <div className="w-full rounded-lg px-32 py-16 flex flex-col items-start justify-center space-y-4 bg-white text-black overflow-hidden">
@@ -32,15 +39,29 @@ function AuthPage() {
                             {isLogin ? "Sign in to continue" : "Create a new account"}
                         </span>
                         <div className="flex flex-col space-y-4 w-full">
-                            {isLogin ? (
-                                <div>
-                                    <Login authData={authData} setAuthData={setAuthData} />
-                                </div>
-                            ) : (
-                                <div>
-                                    <Register authData={authData} setAuthData={setAuthData} />
-                                </div>
-                            )}
+                            <AnimatePresence mode="wait">
+                                {isLogin ? (
+                                    <motion.div
+                                        key="login"
+                                        initial={{ opacity: 0, x: 50 }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                        exit={{ opacity: 0, x: -50 }}
+                                        transition={{ duration: 0.3 }}
+                                    >
+                                        <Login authData={authData} setAuthData={setAuthData} />
+                                    </motion.div>
+                                ) : (
+                                    <motion.div
+                                        key="register"
+                                        initial={{ opacity: 0, x: -50 }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                        exit={{ opacity: 0, x: 50 }}
+                                        transition={{ duration: 0.3 }}
+                                    >
+                                        <Register authData={authData} setAuthData={setAuthData} />
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
                         </div>
 
                         <div className="flex items-baseline mt-8 w-full justify-evenly items-center relative">
@@ -52,6 +73,13 @@ function AuthPage() {
                                     }`}
                             >
                                 Sign In
+                                {isLogin && (
+                                    <motion.div
+                                        layoutId="underline"
+                                        className="absolute left-0 right-0 -bottom-1 h-[3px] bg-[#6b3dcb] rounded"
+                                        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                                    />
+                                )}
                             </button>
                             <div className="border-l border-gray-400 h-5"></div>
                             <button
@@ -62,6 +90,13 @@ function AuthPage() {
                                     }`}
                             >
                                 Sign Up
+                                {!isLogin && (
+                                    <motion.div
+                                        layoutId="underline"
+                                        className="absolute left-0 right-0 -bottom-1 h-[3px] bg-[#6b3dcb] rounded"
+                                        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                                    />
+                                )}
                             </button>
                         </div>
                     </div>
