3d00a51c593615117e7ca50f9592a3f1be6fc60f
nta-wtag <noushin.tasnim@welldev.io>
2025-09-14 16:18:36 +0600

    Make Auth Page responsive

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -11,9 +11,9 @@ const AuthPage = () => {
     const { authData, setAuthData, errors, handleSubmit } = useAuthForm();
 
     return (
-        <div className="w-full h-screen relative flex px-64 text-[#2e214a] font-[poppins]">
-            <div className="w-full flex justify-center h-full">
-                <div className="w-1/2 h-1/2 flex items-center self-center">
+        <div className="w-full h-screen relative flex lg:px-16 xl:px-32 3xl:px-64 text-[#2e214a] font-[poppins]">
+            <div className="w-full h-full flex flex-col lg:flex-row h-full">
+                <div className="w-full lg:w-1/2 h-1/3 lg:h-1/2 flex justify-center self-center">
                     <AnimatePresence mode="wait">
                         <motion.img
                             key={isLogin ? "login-img" : "register-img"}
@@ -22,16 +22,16 @@ const AuthPage = () => {
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.5 }}
-                            className="w-full p-4 h-full object-contain"
+                            className="w-full p-4 h-9/10 lg:h-4/5 object-contain flex items-center self-center"
                         />
                     </AnimatePresence>
                 </div>
-                <div className="w-1/2 h-full flex flex-col items-center justify-center z-10 bg-white">
-                    <div className="w-full rounded-lg px-32 py-16 flex flex-col items-start justify-center space-y-4 bg-white text-black overflow-hidden">
-                        <h1 className="text-5xl mb-4 font-black text-[#6b3dcb] items-center">
+                <div className="w-full lg:w-1/2 h-2/3 lg:h-full flex flex-col items-center lg:justify-center bg-white">
+                    <div className="w-full sm:w-2/3 lg:w-full rounded-lg px-8 lg:px-16 2xl:px-32 py-8 lg:py-16 flex flex-col items-start justify-center lg:space-y-4 bg-white text-black overflow-none">
+                        <h1 className="text-3xl lg:text-5xl mb-2 lg:mb-4 font-black text-[#6b3dcb] items-center">
                             {isLogin ? "Welcome Back" : "Hello There"}
                         </h1>
-                        <span className="mb-8 font-extralight text-lg">
+                        <span className="mb-8 font-extralight text-sm lg:text-lg">
                             {isLogin ? "Sign in to continue" : "Create a new account"}
                         </span>
 
@@ -52,7 +52,7 @@ const AuthPage = () => {
                         <div className="flex items-baseline mt-16 w-full justify-evenly items-center relative">
                             <button
                                 onClick={() => setIsLogin(true)}
-                                className={`relative text-xl px-2 transition-colors text-lg ${!isLogin ? "text-gray-400 font-light" : "text-[#6b3dcb] font-extrabold"
+                                className={`relative text-md lg:text-xl px-2 transition-colors text-lg ${!isLogin ? "text-gray-400 font-light" : "text-[#6b3dcb] font-extrabold"
                                     }`}
                             >
                                 Sign In
@@ -67,7 +67,7 @@ const AuthPage = () => {
                             <div className="border-l border-gray-400 h-5"></div>
                             <button
                                 onClick={() => setIsLogin(false)}
-                                className={`relative text-xl px-2 transition-colors text-lg ${isLogin ? "text-gray-400 font-light" : "text-[#6b3dcb] font-extrabold"
+                                className={`relative text-md lg:text-xl px-2 transition-colors text-lg ${isLogin ? "text-gray-400 font-light" : "text-[#6b3dcb] font-extrabold"
                                     }`}
                             >
                                 Sign Up
