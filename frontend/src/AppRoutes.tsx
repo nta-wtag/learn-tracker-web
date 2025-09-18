2b057d025b0151deba0b6f2688445f4ad118942f
nta-wtag <noushin.tasnim@welldev.io>
2025-09-18 14:27:24 +0600

    Removed unused files

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "@pages/AuthPage";
-import MenteeDashboard from "@pages/Mentee/MenteeDashboard";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
-      <Route path="/mentee" element={<MenteeDashboard />} />
     </Routes>
   );
 }
