f1d47b9dac91ebc35cd99182a1f9f48f927eba9b
nta-wtag <noushin.tasnim@welldev.io>
2025-09-25 16:22:33 +0600

    Fix issues

--- frontend/src/components/auth-components/Auth.tsx
+++ frontend/src/components/auth-components/Auth.tsx
@@ -8,17 +8,17 @@ import loginImg from "assets/Education-Isometric-Illustration.jpg";
 import registerImg from "assets/42B2E4EC-58E6-4E91-9CA5-B570A5634F8F_1_201_a.jpeg";
 
 const Auth = () => {
-  const [isLoggedin, setIsLoggedin] = useState(true);
+  const [isLoginMode, setIsLoginMode] = useState(true);
 
   return (
     <div className="w-full h-screen relative flex lg:px-16 xl:px-32 3xl:px-64 text-[#2e214a] font-[poppins]">
       <div className="w-full h-full flex flex-col lg:flex-row">
-        <AuthImage isLoggedin={isLoggedin} loginImg={loginImg} registerImg={registerImg} />
+        <AuthImage isLoginMode={isLoginMode} loginImg={loginImg} registerImg={registerImg} />
         <div className="w-full lg:w-1/2 h-2/3 lg:h-full flex flex-col items-center lg:justify-center bg-white">
           <div className="w-full sm:w-2/3 lg:w-full rounded-lg px-8 lg:px-16 2xl:px-32 py-8 lg:py-16 flex flex-col items-start justify-center lg:space-y-4 bg-white text-black">
-            <AuthHeader isLoggedin={isLoggedin} />
-            <AuthForm isLoggedin={isLoggedin} />
-            <AuthToggleButton isLoggedin={isLoggedin} setIsLoggedin={setIsLoggedin} />
+            <AuthHeader isLoginMode={isLoginMode} />
+            <AuthForm isLoginMode={isLoginMode} />
+            <AuthToggleButton isLoginMode={isLoginMode} setIsLoginMode={setIsLoginMode} />
           </div>
         </div>
       </div>
--- frontend/src/components/auth-components/AuthToggleButton.tsx
+++ frontend/src/components/auth-components/AuthToggleButton.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import ToggleButton from "components/base-components/ToggleButton";
 
 interface AuthToggleButtonProps {
-  isLoggedin: boolean;
-  setIsLoggedin: (val: boolean) => void;
+  isLoginMode: boolean;
+  setIsLoginMode: (val: boolean) => void;
 }
 
-const AuthToggleButton: React.FC<AuthToggleButtonProps> = ({ isLoggedin, setIsLoggedin }) => (
+const AuthToggleButton: React.FC<AuthToggleButtonProps> = ({ isLoginMode, setIsLoginMode }) => (
   <div className="flex items-baseline mt-16 w-full justify-evenly">
     <ToggleButton 
       label="Sign In" 
-      active={isLoggedin} 
-      onClick={() => setIsLoggedin(true)} 
+      active={isLoginMode} 
+      onClick={() => setIsLoginMode(true)} 
     />
     <div className="border-l border-gray-400 h-5" />
     <ToggleButton 
       label="Sign Up" 
-      active={!isLoggedin} 
-      onClick={() => setIsLoggedin(false)} 
+      active={!isLoginMode} 
+      onClick={() => setIsLoginMode(false)} 
     />
   </div>
 );
