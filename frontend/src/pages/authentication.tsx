c6059fdb0a1fa0873ccf4838dd485bae196e78d6
nta-wtag <noushin.tasnim@welldev.io>
2025-09-25 15:32:49 +0600

    Fix file names

--- frontend/src/pages/authentication.tsx
+++ frontend/src/pages/authentication.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Auth from 'components/auth-components/Auth';
 
 function Authentication(props) {
-    return (
-        <div>
-            Authentication
-        </div>
-    );
+  return (
+    <Auth/>
+  );
 }
 
 export default Authentication;
