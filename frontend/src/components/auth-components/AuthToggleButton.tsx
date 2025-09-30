9ef1568c5d8e22644ee87a5f8cfe8a2938ffb19f
nta-wtag <noushin.tasnim@welldev.io>
2025-09-30 15:08:57 +0600

    Fix conflicts

---
+++ frontend/src/components/auth-components/AuthToggleButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ToggleButton from "components/base-components/ToggleButton";
+
+interface AuthToggleButtonProps {
+  isLoginMode: boolean;
+  setIsLoginMode: (val: boolean) => void;
+}
+
+const AuthToggleButton: React.FC<AuthToggleButtonProps> = ({ isLoginMode, setIsLoginMode }) => (
+  <div className="flex items-baseline mt-16 w-full justify-evenly">
+    <ToggleButton 
+      label="Sign In" 
+      active={isLoginMode} 
+      onClick={() => setIsLoginMode(true)} 
+    />
+    <div className="border-l-2 border-gray-400 h-5" />
+    <ToggleButton 
+      label="Sign Up" 
+      active={!isLoginMode} 
+      onClick={() => setIsLoginMode(false)} 
+    />
+  </div>
+);
+
+export default AuthToggleButton;
