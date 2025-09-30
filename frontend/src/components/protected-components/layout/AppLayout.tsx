714d0114947054961626c3fcb229d8023ceb427d
nta-wtag <noushin.tasnim@welldev.io>
2025-09-30 11:47:43 +0600

    Fix conflicts

--- frontend/src/components/protected-components/layout/AppLayout.tsx
+++ frontend/src/components/protected-components/layout/AppLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout: React.FC = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen bg-gray-50 p-2">
       <div className="flex flex-col flex-1">
-        <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
+        <main className="flex-1 overflow-y-auto p-6 bg-white m-2 rounded-lg shadow-lg">
           <Outlet />
         </main>
       </div>
