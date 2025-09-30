a1c77ae7216ee57358b3861a52d5da95f640a96e
nta-wtag <noushin.tasnim@welldev.io>
2025-09-30 14:41:15 +0600

    Add project setup

---
+++ frontend/src/pages/authentication.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Auth from 'components/auth-components/Auth';
+
+function Authentication(props) {
+  return (
+    <Auth/>
+  );
+}
+
+export default Authentication;
