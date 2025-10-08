6d874658964f665b3af331819bf051fef5bfc451
nta-wtag <noushin.tasnim@welldev.io>
2025-10-08 15:10:10 +0600

    refactor: rename components for clarity and improve code style

--- frontend/src/components/auth-components/Auth.tsx
+++ frontend/src/components/auth-components/Auth.tsx
@@ -1,11 +1,8 @@
 import  React, { useState } from "react";
 import AuthForm from "components/auth-components/AuthForm";
 import AuthHeader from "components/auth-components/AuthHeader";
-import AuthImage from "components/auth-components/AuthImage";
-import AuthToggleButton from "components/auth-components/AuthToggleButton";
-
-import loginImg from "assets/Education-Isometric-Illustration.jpg";
-import registerImg from "assets/42B2E4EC-58E6-4E91-9CA5-B570A5634F8F_1_201_a.jpeg";
+import AuthIllustration from "components/auth-components/AuthIllustration";
+import AuthToggleButton from "components/auth-components/AuthModeSwitcher";
 
 const Auth:React.FC = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
@@ -13,7 +10,7 @@ const Auth:React.FC = () => {
   return (
     <div className="w-full h-screen relative flex lg:px-16 xl:px-32 3xl:px-64 font-[poppins]">
       <div className="w-full h-full flex flex-col lg:flex-row">
-        <AuthImage isLoginMode={isLoginMode} loginImg={loginImg} registerImg={registerImg} />
+        <AuthIllustration isLoginMode={isLoginMode} />
         <div className="w-full lg:w-1/2 h-2/3 lg:h-full flex flex-col items-center lg:justify-center">
           <div className="w-full sm:w-2/3 lg:w-full rounded-lg px-8 lg:px-16 2xl:px-32 py-8 lg:py-16 flex flex-col items-start justify-center lg:space-y-4">
             <AuthHeader isLoginMode={isLoginMode} />
