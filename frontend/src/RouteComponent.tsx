5f978a959d79a89dbf29f7274be09b24f148594d
nta-wtag <noushin.tasnim@welldev.io>
2025-10-08 12:07:37 +0600

    fix: resolve Vercel deployment issues (TS React imports, CSS modules, tsconfig updates)

--- frontend/src/RouteComponent.tsx
+++ frontend/src/RouteComponent.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Authentication from "pages/authentication";
+import type React from "react";
 
-export default function RouteComponent() {
+const RouteComponent:React.FC = () => {
   return (
     <Routes>
       <Route path="/auth" element={<Authentication />} />
     </Routes>
   );
 }
+
+export default RouteComponent;
