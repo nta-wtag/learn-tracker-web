881a7adcc2e85bbfc24279f29c65a472fe70a609
nta-wtag <noushin.tasnim@welldev.io>
2025-10-09 15:45:26 +0600

    refactor: add label with routes

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ROUTES } from 'routes/paths';
 
 const Dashboard: React.FC = () => {
     return (
         <div>
-            Dashboard
+            {ROUTES.DASHBOARD.label}
         </div>
     );
 }
