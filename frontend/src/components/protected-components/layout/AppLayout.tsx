6199190f6c7fe9db21c5dedd0cfde90519c3d2ab
nta-wtag <noushin.tasnim@welldev.io>
2025-10-09 10:53:33 +0600

    refactor: resolve indentation issues

--- frontend/src/components/protected-components/layout/AppLayout.tsx
+++ frontend/src/components/protected-components/layout/AppLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 const AppLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen bg-gray-50 p-2">
-        <main className="flex-1 overflow-y-auto p-6 bg-white m-2 rounded-lg shadow-lg">
-          <Outlet />
-        </main>
+      <main className="flex-1 overflow-y-auto p-6 bg-white m-2 rounded-lg shadow-lg">
+        <Outlet />
+      </main>
     </div>
   );
 };
