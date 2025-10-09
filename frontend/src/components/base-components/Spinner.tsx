0b41138f6862cb72ef0c963258aa12faf531afdd
nta-wtag <noushin.tasnim@welldev.io>
2025-10-09 10:40:58 +0600

    refactor: remove redundant divs

--- frontend/src/components/base-components/Spinner.tsx
+++ frontend/src/components/base-components/Spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner: React.FC = () => (
   <div className="flex items-center justify-center h-screen">
-    <div className="w-12 h-12 border-4 border-primaryColor border-t-transparent rounded-full animate-spin"></div>
+    <div className="w-12 h-12 border-4 border-primaryColor border-t-transparent rounded-full animate-spin"/>
   </div>
 );
 
--- frontend/src/components/protected-components/layout/AppLayout.tsx
+++ frontend/src/components/protected-components/layout/AppLayout.tsx
@@ -4,11 +4,9 @@ import { Outlet } from "react-router-dom";
 const AppLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen bg-gray-50 p-2">
-      <div className="flex flex-col flex-1">
         <main className="flex-1 overflow-y-auto p-6 bg-white m-2 rounded-lg shadow-lg">
           <Outlet />
         </main>
-      </div>
     </div>
   );
 };
