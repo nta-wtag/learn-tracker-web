d64c4443bd5db236351f23b5b7e4173ca8145b12
nta-wtag <noushin.tasnim@welldev.io>
2025-10-15 19:01:59 +0600

    refactor: add enrollmentSlice

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from 'store/slices/authSlice';
 import authUiReducer from 'store/slices/authUiSlice';
 import lessonReducer from 'store/slices/lessonSlice';
+import enrollmentReducer from 'store/slices/enrollmentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     authUi: authUiReducer,
     lesson: lessonReducer,
+    enrollment: enrollmentReducer
   },
 });
 
