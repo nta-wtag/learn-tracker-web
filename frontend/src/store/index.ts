cb5c353f130553f6a54549f219600bfd9b07d3ad
nta-wtag <noushin.tasnim@welldev.io>
2025-10-20 11:27:40 +0600

    remove redundant file

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from 'store/slices/authSlice';
-import coursesReducer from 'store/slices/courseSlice';
 import lessonReducer from 'store/slices/lessonSlice';
 import enrollmentReducer from 'store/slices/enrollmentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    courses: coursesReducer,
     lesson: lessonReducer,
     enrollment: enrollmentReducer
   },
